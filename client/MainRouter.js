c9f7eb85c38e52e39f113581a2281b0ac38fadf2
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 14:25:53 -0500

    Update MainRouter.js

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,1 +1,13 @@
-MainRouter.js
+import React, {Component} from 'react'
+import {Route, Switch} from 'react-router-dom'
+import Home from './core/Home'
+class MainRouter extends Component {
+  render() {
+    return (<div>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+    </div>)
+  }
+}
+export default MainRouter
