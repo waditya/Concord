0f2d75c1af6ad34055d09522e9e3607178733d4b
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 15:44:43 -0500

    Update Users.js

--- client/user/Users.js
+++ client/user/Users.js
@@ -1,3 +1,43 @@
 class Users extends Component {
   state = { users: [] }
-...
+    componentDidMount = () => {
+    list().then((data) => {
+      if (data.error)
+        console.log(data.error)
+      else
+        this.setState({users: data})
+    })
+  }
+  
+  render() {
+    const {classes} = this.props
+    return (
+      <Paper className={classes.root} elevation={4}>
+        <Typography type="title" className={classes.title}>
+          All Users
+        </Typography>
+        <List dense>
+          {this.state.users.map(function(item, i) {
+              return <Link to={"/user/" + item._id} key={i}>
+                <ListItem button="button">
+                  <ListItemAvatar>
+                    <Avatar>
+                      <Person/>
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={item.name}/>
+                  <ListItemSecondaryAction>
+                    <IconButton>
+                      <ArrowForward/>
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              </Link>
+            })}
+        </List>
+      </Paper>
+    )
+}
+}
+  
+  
