f10a66501f74bf87eff49601649293160790568f
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-15 21:01:00 -0500

    Create template.js

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+    return `<!doctype html>
+      <html lang="en">
+          <head>
+             <meta charset="utf-8">
+             <title>Concord -- Your Social Network</title>
+          </head>
+          <body>
+            <div id="root">Hello World</div>
+          </body>
+      </html>`
+}
