b9bda26271195d414c0e739ed83b4ece80cbbc89
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-15 21:03:32 -0500

    Create express.js

---
+++ server/express.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+import Template from './../template'
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
+
+const app = express()
+	/* Configure Express*/
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+//Handle authentication related erros thrown by express-jwt while JWT token validation during incoming requests
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"Authentication error" : err.name + ": " + err.message})
+  }
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+
+export default app
