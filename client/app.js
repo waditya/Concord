915414d92c6f2ac76cd9fa435cd58dc66d277853
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 14:02:43 -0500

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,1 +1,23 @@
-app.js
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles'
+import {indigo, pink} from 'material-ui/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+    light: '#757de8',
+    main: '#3f51b5',
+    dark: '#002984',
+    contrastText: '#fff',
+  },
+  secondary: {
+    light: '#ff79b0',
+    main: '#ff4081',
+    dark: '#c60055',
+    contrastText: '#000',
+  },
+    openTitle: indigo['400'],
+    protectedTitle: pink['400'],
+    type: 'light'
+  }
+})
+
