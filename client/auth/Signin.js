e1175d1bcc2d5478a7457e79666ad64188b30525
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 16:18:59 -0500

    Update Signin.js

--- client/auth/Signin.js
+++ client/auth/Signin.js
@@ -1,3 +1,29 @@
 class Signin extends Component {
   state = { email: '', password: '', error: '', redirectToReferrer: false } 
-...
+  clickSubmit = () => {
+    const user = {
+      email: this.state.email || undefined,
+      password: this.state.password || undefined
+    }
+    signin(user).then((data) => {
+      if (data.error) {
+        this.setState({error: data.error})
+      } else {
+auth.authenticate(data, () => {
+          this.setState({redirectToReferrer: true})
+        })
+      }
+    })
+}
+
+render() {
+    const {classes} = this.props
+ const {from} = this.props.location.state || {
+      from: {pathname: '/' }
+    } 
+    const {redirectToReferrer} = this.state
+    if (redirectToReferrer)
+      return (<Redirect to={from}/>)
+    return (...)
+  }
+}
