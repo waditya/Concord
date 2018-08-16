33d74274b920ce3a3236097cd225bfa56654f90b
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-15 21:08:16 -0500

    Create user.controller.js

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,86 @@
+import User from '../models/user.model'
+import _ from 'lodash'
+import errorHandler from '../helpers/dbErrorHandler'
+
+//Callback method for creating a user
+
+const create = (req, res, next) => {
+  const user = new User(req.body)
+  user.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+    res.status(200).json({
+      message: "Successfully signed up!"
+    })
+  })
+}
+const list = (req, res) => {
+  User.find((err, users) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+    res.json(users)
+  }).select('name email updated created')
+}
+
+/*The userByID controller function uses the value in the :userId param to query the database by _id, 
+and load the matching user's details.*/
+
+const userByID = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err || !user)
+      return res.status('400').json({
+        error: "User not found"
+      })
+    req.profile = user
+    next()
+  })
+}
+
+/*This controller functions takes the userID parameter from the get method and returns the user profile and 
+renders - displays it using read controller*/
+
+const read = (req, res) => {
+  req.profile.hashed_password = undefined
+  req.profile.salt = undefined
+  return res.json(req.profile)
+}
+
+/*This controller function reads the userID parameter and updates the user*/
+
+const update = (req, res, next) => {
+  let user = req.profile
+  user = _.extend(user, req.body)
+  user.updated = Date.now()
+  user.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+    user.hashed_password = undefined
+    user.salt = undefined
+    res.json(user)
+  })
+}
+/*This controller function loads the user by userID and then removes the user*/
+const remove = (req, res, next) => {
+  let user = req.profile
+  user.remove((err, deletedUser) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+    deletedUser.hashed_password = undefined
+    deletedUser.salt = undefined
+    res.json(deletedUser)
+  })
+}
+
+export default { create, userByID, read, list, remove, update }
