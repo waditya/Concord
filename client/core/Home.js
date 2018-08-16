a61ff0fc06d16f55c35911da739fc5dbdd173e95
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 15:32:08 -0500

    Create Home.js

---
+++ client/core/Home.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import Card, {CardContent, CardMedia} from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import seashellImg from './../assets/images/seashell.jpg'
+
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+}) 
+
+class Home extends Component {
+  render() {
+    const {classes} = this.props 
+    return (
+      <div>
+        <Card className={classes.card}>
+          <Typography type="headline" component="h2" className=
+          {classes.title}>
+            Home Page
+          </Typography>
+          <CardMedia className={classes.media} image={seashellImg} 
+          title="Unicorn Shells"/>
+          <CardContent>
+            <Typography type="body1" component="p">
+              Welcome to the Mern Skeleton home page
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+/*export the component with the defined styles passed in using withStyles from Material-UI. 
+Using withStyles like this creates a Higher-order component (HOC) that has access to the 
+defined style objects as props.*/
+
+export default withStyles(styles)(Home)
