d5c8f072ae725d613066974e1678d2d2919775e9
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 13:46:51 -0500

    Update template.js

--- template.js
+++ template.js
@@ -6,6 +6,7 @@ export default () => {
              <title>Concord -- Your Social Network</title>
           </head>
           <body>
+			<div id="root"></div><script type="text/javascript" src="/dist/bundle.js"></script>
             <div id="root">Hello World</div>
           </body>
       </html>`
