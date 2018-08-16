deacd4514d232379e37f8c772ea0032dc19be3f2
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-16 15:32:40 -0500

    Create api-user.js

---
+++ client/user/api-user.js
@@ -0,0 +1,13 @@
+const create = (user) => {
+  return fetch('/api/users/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then((response) => {
+      return response.json()
+    }).catch((err) => console.log(err))
+}
