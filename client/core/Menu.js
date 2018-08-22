b2c3b1e2abb8d265621da171c97af6fc7b8ab735
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-22 17:27:56 -0500

    Update Menu.js

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -15,3 +15,10 @@ const Menu = withRouter(({history}) => (<div>
     </Toolbar>
   </AppBar>
 </div>))
+
+const isActive = (history, path) => {
+  if (history.location.pathname == path)
+    return {color: '#ff4081'}
+  else
+    return {color: '#ffffff'}
+}
