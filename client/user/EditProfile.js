81cc69946d2a1df4a4a67f9d59c848ae751493ce
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-22 17:20:44 -0500

    Update EditProfile.js

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -1,1 +1,19 @@
-EditProfile.js
+clickSubmit = () => {
+    const jwt = auth.isAuthenticated()
+    const user = {
+      name: this.state.name || undefined,
+      email: this.state.email || undefined,
+      password: this.state.password || undefined
+    }
+    update({
+      userId: this.match.params.userId
+    }, {
+      t: jwt.token
+    }, user).then((data) => {
+      if (data.error) {
+        this.setState({error: data.error})
+      } else {
+        this.setState({'userId': data._id, 'redirectToProfile': true})
+      }
+    })
+}
