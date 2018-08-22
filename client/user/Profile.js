cb6cd8a8101bf14b9d6fb007c2912ee7c7bc979c
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-22 17:06:21 -0500

    Update Profile.js

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -3,4 +3,16 @@ class Profile extends Component {
     super()
     this.state = { user: '', redirectToSignin: false }
     this.match = match 
-  } ...
+  } 
+  
+  init = (userId) => {
+    const jwt = auth.isAuthenticated()
+    read({
+      userId: userId
+    }, {t: jwt.token}).then((data) => {
+      if (data.error)
+        this.setState({redirectToSignin: true})
+      else
+        this.setState({user: data})
+    })
+}
