c8bb8f7f00873c9ebefad43ab5f6d7191d8e2104
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-22 17:30:16 -0500

    Update MainRouter.js

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -5,12 +5,14 @@ import Home from './core/Home'
 class MainRouter extends Component {
   render() {
     return (<div>
+	  </Menu>
       <Switch>
         <Route exact path="/" component={Home}/>
 		<Route path="/users" component={Users}/>
 		<Link to="/users">Users</Link>
 		<Route path="/signup" component={Signup}/>
 		<Route path="/signin" component={Signin}/>
+		<PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
 		<Route path="/user/:userId" component={Profile}/>
       </Switch>
     </div>)
