e29407a5291749bde1ff73f0c26491d0e07398b7
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-25 20:09:21 -0500

    Update auth-helper.js

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -1,10 +1,7 @@
-authenticate(jwt, cb) {
-    if(typeof window !== "undefined")
-        sessionStorage.setItem('jwt', JSON.stringify(jwt))
-    cb()
-}
+import { signout } from './api-auth.js'
 
-isAuthenticated() {
+const auth = {
+  isAuthenticated() {
     if (typeof window == "undefined")
       return false
 
@@ -12,14 +9,21 @@ isAuthenticated() {
       return JSON.parse(sessionStorage.getItem('jwt'))
     else
       return false
+  },
+  authenticate(jwt, cb) {
+    if (typeof window !== "undefined")
+      sessionStorage.setItem('jwt', JSON.stringify(jwt))
+    cb()
+  },
+  signout(cb) {
+    if (typeof window !== "undefined")
+      sessionStorage.removeItem('jwt')
+    cb()
+    //optional
+    signout().then((data) => {
+      document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+    })
+  }
 }
 
-signout(cb) {
-      if(typeof window !== "undefined")
-        sessionStorage.removeItem('jwt')
-      cb()
-      signout().then((data) => {
-          document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00 
-          UTC; path=/;"
-      })
-}
+export default auth
